b9dff7c32fef41e93afff021d72509ae3266268c
Nirab <me.nirav11@gmail.com>
2020-04-26 08:12:49 +0545

    Upto Downstream

--- test.py
+++ test.py
@@ -22,26 +22,28 @@ cfg.PATHS['working_dir'] = utils.gettempdir(dirname='/home/nirab/OGGM/OGGM-Getti
 
 rgi_ids = ['RGI60-11.01328', 'RGI60-11.00897']
 
-#gdirs = workflow.init_glacier_directories(rgi_ids, from_prepro_level=2, prepro_border=80)
 #graphics.plot_domain(gdirs, figsize=(8, 7))
 #gdir = rgi_ids[1]
-gdir= []
+
 # Plotting the glacier over the DEM file
 for rgi in rgi_ids:
 
     f, axs = plt.subplots(2, 2, figsize=(8, 6))
     for ax, border in zip(np.array(axs).flatten(), [10, 80, 160, 250]):
-        gdirs = workflow.init_glacier_directories(rgi,
+        gdir = workflow.init_glacier_directories(rgi,
                                                     from_prepro_level=1,prepro_border=border)
 
-        graphics.plot_domain(gdirs, ax=ax, title='Border: {}'.format(border),
+        graphics.plot_domain(gdir, ax=ax, title='Border: {}'.format(border),
                                 add_colorbar=False,
                                 lonlat_contours_kwargs={'add_tick_labels':False})
-    gdir = gdir + gdirs                                           
     print("dir==",gdir)
-plt.show()
+
+
+gdirs = workflow.init_glacier_directories(rgi_ids, from_prepro_level=2, prepro_border=80)
+
+
 # run the glacier_masks task on all gdirs
-#workflow.execute_entity_task(tasks.glacier_masks, gdirs)
+workflow.execute_entity_task(tasks.glacier_masks, gdirs)
 
 #gdir = gdirs[1]
 #print('Path to the DEM:', gdir.get_filepath('dem'))
@@ -51,7 +53,7 @@ plt.show()
 # Path to the masks: /home/nirab/OGGM/OGGM-GettingStarted/per_glacier/RGI60-11/RGI60-11.00/RGI60-11.00897/gridded_data.nc
 
 # Apply several tasks sequentially (i.e. one after an other) on our glacier list
-""" list_talks = [
+list_talks = [
          tasks.compute_centerlines, #Compute the centerlines following Kienholz et al., (2014).
          tasks.initialize_flowlines, #Computes more physical Inversion Flowlines from geometrical Centerlines
          tasks.compute_downstream_line, #Computes the Flowline along the unglaciated downstream topography
@@ -90,4 +92,4 @@ plt.show()
 #  so that the total glacier area and elevation distribution is conserved
 
 
- """
+ 
